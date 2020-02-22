9a9c34fa7560c11dc6f6a709edb5ca3e8226cf41
lambinvoker <trotter.justin@gmail.com>
2020-02-22 15:24:32 -0600

    Added unity test CLI in .gitlab-ci.yml

--- Assets/Tests/MovementSystem_Test.cs
+++ Assets/Tests/MovementSystem_Test.cs
@@ -4,7 +4,7 @@ using Unity.Entities.Tests;
 
 namespace Tests {
     [TestFixture]
-    [Category("2D Toolbox Tests")]
+    [Category("2DToolbox")]
     public class MovementSystem_Test: ECSTestsFixture  {
         [Test]
        public void WhenDeadAndNEquals3() {
