d699a27c39c4172fdb35c597a556b5ef3d61733c
lambinvoker <trotter.justin@gmail.com>
2020-02-25 15:02:45 -0600

    set up playMode tests

--- Assets/Scripts/Tests/EditMode Tests/MovementSystem_Test.cs
+++ Assets/Scripts/Tests/EditMode Tests/MovementSystem_Test.cs
@@ -2,13 +2,13 @@
 using Unity.Entities;
 using Unity.Entities.Tests;
 
-namespace Tests {
+namespace EditModeTests {
     [TestFixture]
     [Category("2DToolbox")]
     public class MovementSystem_Test: ECSTestsFixture  {
         [Test]
        public void WhenDeadAndNEquals3() {
-           Assert.Fail();
+           //Assert.Fail();
        }
     }
 }
---
+++ Assets/Scripts/Tests/PlayMode Tests/PlayModeTest_Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Unity.Entities;
+using Unity.Entities.Tests;
+
+namespace PlayModeTests {
+    [TestFixture]
+    [Category("2DToolbox")]
+    public class PlayModeTest_Test: ECSTestsFixture  {
+        [Test]
+       public void WhenDeadAndNEquals3() {
+           //Assert.Fail();
+       }
+    }
+}
