2710fbf5fa9c50a0765c2e1c87b0abe40fd44774
Justin Trotter <trotter.justin@gmail.com>
2021-09-05 07:20:46 -0400

    clear out all dots stuff

--- Assets/Scripts/Data Components/MovementData.cs
+++
@@ -1,9 +0,0 @@
-﻿using Unity.Entities;
-using UnityEngine;
-
-[GenerateAuthoringComponent]
-public struct MovementData : IComponentData
-{
-    public Vector3 direction;
-    public float speed;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Unity-2D-Toolbox
-An attempt to create a set of components and systems useful for general 2D game development on Unity
